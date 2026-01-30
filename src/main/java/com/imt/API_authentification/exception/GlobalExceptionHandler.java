b9c7ed2237f26ad3be3814e0adbd8e07bea36b5a
mathismr <mathis10r@outlook.fr>
2026-01-30 13:46:32 +0100

    Added error handling

---
+++ src/main/java/com/imt/API_authentification/exception/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.imt.API_authentification.exception;
+
+import jakarta.xml.bind.ValidationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.ArrayList;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<Errors> handleValidationException(Exception ex) {
+        Errors errors = new Errors(new ArrayList<>());
+        CustomError customError = new CustomError(
+                400,
+                ex.getMessage()
+        );
+        errors.addError(customError);
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(HttpClientErrorException.NotFound.class)
+    public ResponseEntity<Errors> handleNotFoundException(Exception ex) {
+        Errors errors = new Errors(new ArrayList<>());
+        CustomError customError = new CustomError(
+                404,
+                ex.getMessage()
+        );
+        errors.addError(customError);
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
